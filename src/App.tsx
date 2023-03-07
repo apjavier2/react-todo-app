edb571b06f60e0123001932cee34e2a1577236a9
apjavier2 <apjavier27@gmail.com>
2023-03-07 12:55:54 +0800

    created todo model

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Todos from "./components/Todos";
+import Todo from "./models/todo";
 
 function App() {
+  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+
   return (
     <div>
-      <Todos items={["Learn react", "Learn TS"]} />
+      <Todos items={todos} />
     </div>
   );
 }
--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import Todo from "../models/todo";
 
 //React.FC - generic react type
 //merge our declared type to the react.FC
-const Todos: React.FC<{ items: string[] }> = (props) => {
+const Todos: React.FC<{ items: Todo[] }> = (props) => {
   return (
     <ul>
       {props.items.map((todo) => (
-        <li>{todo}</li>
+        <li key={todo.id}>{todo.text}</li>
       ))}
     </ul>
   );
