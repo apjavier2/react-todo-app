7cf3eefd59d24cb54aa8ab9f3471049a86ea4609
apjavier2 <apjavier27@gmail.com>
2023-03-07 14:01:52 +0800

    added delete functionality in todo items

--- src/components/Todos.tsx
+++ src/components/Todos.tsx
@@ -5,11 +5,18 @@ import classes from "./Todos.module.css";
 
 //React.FC - generic react type
 //merge our declared type to the react.FC
-const Todos: React.FC<{ items: Todo[] }> = (props) => {
+const Todos: React.FC<{
+  items: Todo[];
+  onRemoveTodo: (todoId: string) => void;
+}> = (props) => {
   return (
     <ul className={classes.list}>
       {props.items.map((todo) => (
-        <TodoItem key={todo.id} text={todo.text} />
+        <TodoItem
+          key={todo.id}
+          text={todo.text}
+          onRemoveTodo={props.onRemoveTodo.bind(null, todo.id)}
+        />
       ))}
     </ul>
   );
