8ca231137dd382d9fc3a5519b6890fff870ccb26
apjavier2 <apjavier27@gmail.com>
2023-03-07 13:28:02 +0800

    implement adding of dynamic data in the todo list

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,23 @@
+import { useState } from "react";
 import "./App.css";
+import NewTodo from "./components/NewTodo";
 import Todos from "./components/Todos";
 import Todo from "./models/todo";
 
 function App() {
-  const todos = [new Todo("Learn React"), new Todo("Learn TypeScript")];
+  //<Todo[]> indicates that this state will receive a list of Todo in the future
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
+  };
 
   return (
     <div>
+      <NewTodo onAddTodo={addTodoHandler} />
       <Todos items={todos} />
     </div>
   );
---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,26 @@
+import React, { useRef } from "react";
+
+//{onAddTodo: (enteredText: string)=> void}
+//indicates that we are expecting a function that takes string as a parameter and returns void
+const NewTodo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
+  //This indicates that the ref that this will be connected to is for an input element.
+  const todoTextInputRef = useRef<HTMLInputElement>(null);
+  const submitHandler = (event: React.FormEvent) => {
+    event.preventDefault();
+    const enteredText = todoTextInputRef.current!.value;
+    if (enteredText.trim().length === 0) {
+      return;
+    }
+    props.onAddTodo(enteredText);
+  };
+
+  return (
+    <form onSubmit={submitHandler}>
+      <label htmlFor="text">Todo Text</label>
+      <input type="text" id="text" ref={todoTextInputRef}></input>
+      <button>Add todo</button>
+    </form>
+  );
+};
+
+export default NewTodo;
